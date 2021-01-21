21d22f4378e292288f84d72d6e7c7cdd210eb45c
Yulia Lime <lime666lime@gmail.com>
2021-01-21 23:07:49 +0200

    friendly category url

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,7 @@
 class ProductsController < ApplicationController
 
+  def show
+    @product = @category.product.find(params[:id])
+  end
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
   validates :title, presence: true
   has_many :products
+
+  def to_param
+    "#{id}-#{title.gsub(/[^a-z0-9]+/i, '-')}"
+  end
 end
